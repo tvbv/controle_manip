9550df0678e350577f59ba8f3fbc7758e7ca5295
tvbv <thomas.bersani--veroni@ens-paris-saclay.fr>
2022-02-15 13:32:40 +0100

    keithley2230GDriver 1.0 finished

--- lab/.ipynb_checkpoints/keith2230GDriver-checkpoint.py
+++ lab/.ipynb_checkpoints/keith2230GDriver-checkpoint.py
@@ -14,29 +14,44 @@ import time
 
 class Keith2230G():
     
-    def __init__(self, adress, backend = '@ivi',sleep_time = 0.35):
+    def __init__(self, adress, backend = '@ivi', reset = 0 ,silence_initial_measurements=0):
         """
-        Initializes the instrument with instrument address, backend to use with pyvisa and sleep time to wait before each measure
-        Parameters
+        Initializes the instrument with instrument address, backend to use with pyvisa,reset and silencing initial measurements
         ----------
         inst_address : str
             The port address of the instrument.
         backend : str
             Backend to use for pyvisa
-        sleep_time : float
-            time (in secs) to wait before a measure, because of the volt and current setting inertia
+        reset : bool
+            resets every parameters to default setup (see 4-11 on keithley manual) and set every channel to 0V and 0A
+        silence_initial_measurements :bool
+            to silence 
         """
-        self.sleep_time = sleep_time
+
         self.adress = adress
         self.ressource_manager = visa.ResourceManager(backend)
         self.inst = self.ressource_manager.open_resource(self.adress)
+        self.inst.write("SYST:REM \n") #put the keithley in remote mode
         
+        if reset:
+            self.inst.write("*RST \n")
+            for i in ['CH1','CH2','CH3']:
+                self.set_channel_voltage(i,0)
+                self.set_channel_current(i,0)
+        
+        print("Connected Device : " + self.inst.query("*IDN? \n"))
+        
+        if not(silence_initial_measurements):
+            self.reality_check()
+        
+        
+    
     def get_channel(self):
         """Queries selected channel."""
         channel = self.inst.query("INST?\n")
         return(channel)
     
-    def get_channel_curr(self, channel):
+    def get_channel_current(self, channel):
         """
         Queries the current reading on the specified channel.
         Parameters
@@ -45,15 +60,13 @@ class Keith2230G():
             The selected channel or channels with the current readings to
             return. can be CH1, CH2, CH3 or ALL
         """
-        time.sleep(self.sleep_time)
-        
         if channel in ['CH1','CH2','CH3','ALL']:
             reading = self.inst.query(f"FETC:CURR? {channel}\n")
         else:
             raise ValueError("Value Error. Please enter a valid channel.")
         return(reading)
     
-    def get_channel_volt(self, channel):
+    def get_channel_voltage(self, channel):
         """
         Queries the voltage reading on the specified channel.
         Parameters
@@ -62,15 +75,45 @@ class Keith2230G():
             The selected channel or channels with the voltage readings to
             return. can be CH1, CH2, CH3 or ALL.
         """
-        time.sleep(self.sleep_time)
-
         if channel in ['CH1','CH2','CH3','ALL']:
             volt = self.inst.query(f"FETC:VOLT? {channel}\n")
         else:
             raise ValueError("Value Error. Please enter a valid channel.")
         return(volt)
     
-    def set_channel_curr(self, channel, curr):
+    def get_channel_current_set(self, channel):
+        """
+        Queries the current set on the specified channel.
+        Parameters
+        ----------
+        channel : str
+            The selected channel or channels with the current set to
+            return. can be CH1, CH2, CH3 or ALL
+        """
+        if channel in ['CH1','CH2','CH3','ALL']:
+            self.inst.write(f"INST:SEL {channel}\n")
+            reading = self.inst.query("SOUR:CURR?\n")
+        else:
+            raise ValueError("Value Error. Please enter a valid channel.")
+        return(reading)
+    
+    def get_channel_voltage_set(self, channel):
+        """
+        Queries the voltage set on the specified channel.
+        Parameters
+        ----------
+        channel : str
+            The selected channel or channels with the voltage set to
+            return. can be CH1, CH2, CH3 or ALL.
+        """
+        if channel in ['CH1','CH2','CH3','ALL']:
+            self.inst.write(f"INST:SEL {channel}\n")
+            volt = self.inst.query("SOUR:VOLT?\n")
+        else:
+            raise ValueError("Value Error. Please enter a valid channel.")
+        return(volt)
+    
+    def set_channel_current(self, channel, curr):
         """
         set the current on the specified channel.
         Parameters
@@ -80,16 +123,14 @@ class Keith2230G():
             return. can be CH1, CH2 or CH3.
         curr : the current in Amps
         """
-        
-        
+           
         if channel in ['CH1','CH2','CH3']:
             self.inst.write(f"INST:SEL {channel}\n")
-            time.sleep(self.sleep_time)
             self.inst.write(f"SOUR:CURR {curr} A\n")
         else:
             raise ValueError("Value Error. Please enter a valid channel.")
             
-    def set_channel_volt(self, channel, volt):
+    def set_channel_voltage(self, channel, volt):
         """
         set the voltage on the specified channel.
         Parameters
@@ -99,11 +140,8 @@ class Keith2230G():
             set. can be CH1, CH2 or CH3.
         voltage : the voltage in Volts
         """
-        
-
         if channel in ['CH1','CH2','CH3']:
             self.inst.write(f"INST:SEL {channel}\n")
-            time.sleep(self.sleep_time)
             if channel == 'CH3' and volt>6.1:
                 raise  ValueError("Value Error. Please enter a voltage below 6.1 for channel 3.")
             else:
@@ -125,10 +163,34 @@ class Keith2230G():
 
         if channel in ['CH1','CH2','CH3']:
             self.inst.write(f"INST:SEL {channel}\n")
-            time.sleep(self.sleep_time)
             if state in [0,1]:
-                self.inst.write(f"OUTP:ENAB {state}\n")
+                self.inst.write(f"SOUR:CHAN:OUTP {state}\n")
             else:
                 raise ValueError("Value Error. Please enter a valid output state")
         else:
             raise ValueError("Value Error. Please enter a valid channel.")
+    
+    def reality_check(self):
+        """
+        Print the set and measured current and voltage of every channel.
+        ----------
+        """
+        for i in ['CH1','CH2','CH3']:
+            print("=====\n"+"Channel : "+i[2]+"; Set voltage = "+self.get_channel_voltage_set(i)+"; Set Current = "+self.get_channel_current_set(i)+"\nMeasured voltage  = "+self.get_channel_voltage(i)+"; Measured current = "+self.get_channel_current(i))
+        print("=====")
+    
+    def channel_reality_check(self,channel):
+        """
+        Print the set and measured current and voltage of a specified channel.
+        ----------
+        channel : str
+            The selected channel.
+        """
+        if channel in ['CH1','CH2','CH3']:
+            print("=====\n"+"Channel : "+channel[2]+"\nSet voltage = "+self.get_channel_voltage_set(channel)+"\nSet Current = "+self.get_channel_current_set(channel)+"\nMeasured voltage  = "+self.get_channel_voltage(channel)+"\nMeasured current = "+self.get_channel_current(channel))
+        print("=====")
+        
+    def close(self):
+        self.inst.write('SYST:LOC')
+        self.inst.close()
+        
