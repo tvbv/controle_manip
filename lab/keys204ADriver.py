f3d74489b819f09594659cc7e285ce5391addd18
tvbv <thomas.bersani--veroni@ens-paris-saclay.fr>
2022-03-15 18:02:04 +0100

    adding of the keysight s series 204A driver

---
+++ lab/keys204ADriver.py
@@ -0,0 +1,186 @@
+import sys
+import pyvisa as visa
+import time
+from comtypes.client import GetModule
+from comtypes.client import CreateObject
+from comtypes.automation import VARIANT
+# Run GetModule once to generate comtypes.gen.VisaComLib.
+if not hasattr(sys, "frozen"):
+   GetModule("C:\Program Files (x86)\IVI Foundation\VISA\VisaCom\GlobMgr.dll")
+import comtypes.gen.VisaComLib as VisaComLib
+
+
+class Keys204A():
+    
+    def __init__(self, adress, backend = '@ivi'):
+        """
+        Initializes the instrument with instrument address, backend to use with pyvisa
+        ----------
+        inst_address : str
+            The port address of the instrument.
+        backend : str
+            Backend to use for pyvisa
+        """
+        
+        self.adress = adress
+        
+        self.ressource_manager = CreateObject("VISA.GlobalRM", \
+        interface=VisaComLib.IResourceManager)
+        self.inst = CreateObject("VISA.BasicFormattedIO", \
+        interface=VisaComLib.IFormattedIO488)
+        self.inst.IO = self.ressource_manager.Open(adress)
+        # Clear the interface.
+        self.inst.IO.Clear
+        
+        
+        print("Connected Device : " + self.__do_query_string("*IDN?"))
+    
+    def __do_command(self,command):
+        self.inst.WriteString("%s" % command, True)
+        self.__check_instrument_errors(command)
+        
+    def __do_query_string(self,query):
+        """
+        private method, enables querying easily the scope
+        ----------
+        query : str
+            The query to send to the scope
+        """
+        self.inst.WriteString("%s" % query, True)
+        result = self.inst.ReadString()
+        self.__check_instrument_errors(query)
+        return result
+    
+    def __check_instrument_errors(self,command):
+        """
+        private method, enables detection of errors
+        ----------
+        command : str
+            The command passed to the scope
+        """
+        while True:
+            self.inst.WriteString(":SYSTem:ERRor? STRing", True)
+            error_string = self.inst.ReadString()
+            if error_string: # If there is an error string value.
+                if error_string.find("0,", 0, 2) == -1: # Not "No error".
+                    print("ERROR: %s, command: '%s'" % (error_string, command))
+                    print("Exited because of error.")
+                    sys.exit(1)
+                else: # "No error"
+                    break
+
+            else: # :SYSTem:ERRor? STRing should always return string.
+                print("ERROR: :SYSTem:ERRor? STRing returned nothing, command: '%s'"% command)
+                print("Exited because of error.")
+                sys.exit(1)
+    
+    def set_trigger(self,trigger_channel = 1,trigger_sweep = None ,trigger_level = None, print_output = 0):
+        """
+        set the scope's trigger mode, level and channel 
+        ----------
+        trigger_sweep : str
+            The type of trigger. "TRIGGERED" or "AUTO". By default no changes are applied.
+        trigger_level : float
+            the level for the trigger. By default no changes are applied.
+        trigger_channel : int
+            the channel on wich the trigger is set.
+        print_output : boolean
+            wether to prit or not the new setting to verify all is good
+        """
+        
+        if trigger_sweep != None:
+            self.__do_command(":TRIGger:SWEep "+trigger_sweep)
+
+        self.__do_command(":TRIGger:EDGE:SOURce CHANnel" + str(trigger_channel))
+        
+        if trigger_level !=None:
+            self.__do_command(":TRIGger:LEVel CHANnel" + str(trigger_channel)+","+str(trigger_level))
+
+        
+        if print_output == 1:
+            qresult = self.__do_query_string(":TRIGger:SWEep?")
+            print("Trigger sweep: %s" % qresult)
+            qresult = self.__do_query_string(":TRIGger:EDGE:SOURce?")
+            print("Trigger edge source: %s" % qresult)
+            qresult = self.__do_query_string(":TRIGger:LEVel? CHANnel"+ str(trigger_channel))
+            print("Trigger level, channel" + str(trigger_channel)+": %s" % qresult)
+    
+    def time_base(self,time_scale, time_ref,print_output = 0):
+        """
+        set the scope's time base scale and offset
+        ----------
+        time_scale : float
+            time per division in seconds. ranges from 5e-12 to 20.
+        time_ref : int
+            percentage of the screen (starting from the left) where the time offset is put. 0 = flush left, 50 = middle , 100 = flush right.
+        print_output : boolean
+            wether to prit or not the new setting to verify all is good
+        """
+        # Set horizontal scale and offset.
+        self.__do_command(":TIMebase:SCALe " + str(time_scale))
+        self.__do_command(":TIMebase:POSition 0.0")
+        self.__do_command(":TIMebase:REFerence:PERCent " + str(time_ref))
+        
+        if print_output == 1:
+            qresult = self.__do_query_string(":TIMebase:SCALe?")
+            print("Timebase scale: %s" % qresult)
+            qresult = self.__do_query_string(":TIMebase:POSition?")
+            print("Timebase position: %s" % qresult)
+            qresult = self.__do_query_string(":TIMebase:REFerence:PERCent?")
+            print(" Timebase reference percent: %s" %qresult)
+                          
+    def set_channels(self, channels=[1,2,3,4], displays = [1,1,1,1], y_scales = [1,1,1,1], offsets = [0,0,0,0], probes = [None,None,None,None] , input_couplings = ["DC",'DC','DC','DC'],print_output = 0):
+        """
+        set the scope's time base scale and offset
+        ----------
+        channels : int list
+            channels to tweak. 1 to 4.
+        displays : boolean list
+            displaying state of the specified channels.
+        y_scales : int list
+            voltage per division for the specifieds channels.
+        offsets : real list
+            offset for the specified channel
+        probes : float list
+            probes ratio (X:1) for the specified channels : ranges from 1e-4 to 1e3
+        input_coupling : str list
+            choose from the following parameters :
+                DC = DC coupling, 1 MΩ impedance.
+                DC50 or DCFifty = DC coupling, 50Ω impedance.
+                AC = AC coupling, 1 MΩ impedance.
+                LFR1 or LFR2 = AC 1 MΩ input impedance.
+            When no probe is attached, the coupling for each channel can be AC, DC,
+            DC50, or DCFifty.
+            If you have an 1153A probe attached, the valid parameters are DC, LFR1, and
+            LFR2 (low-frequency reject).
+        print_output : boolean
+            wether to prit or not the new setting to verify all is good
+        """
+        for i in range(len(channels)):
+            c = channels[i]
+            print(i)
+            if probes[i] != None : 
+                self.__do_command(":CHANnel"+ str(c)+":PROBe "+str(probes[i]))
+            if displays[i] != None :
+                self.__do_command(":CHANnel"+ str(c)+":DISPlay "+str(displays[i]))
+            if y_scales[i] != None :
+                self.__do_command(":CHANnel" +str(c)+ ":SCALe " + str(y_scales[i]))
+            
+            if offsets[i] != None :
+                self.__do_command(":CHANnel" +str(c)+ ":OFFSet "+str(offsets[i]))
+            
+            if input_couplings[i] != None :
+                self.__do_command(":CHANnel"+ str(c)+":INPut "+ input_couplings[i])
+            
+            if print_output == 1:
+                qresult = self.__do_query_string(":CHANnel"+ str(c)+":PROBe?")
+                print("Channel "+ str(c)+" probe attenuation factor: %s" % qresult)
+                qresult = self.__do_query_string(":CHANnel"+ str(c)+":DISPlay?")
+                print("Channel "+ str(c)+" display: %s" % qresult)
+                qresult = self.__do_query_string(":CHANnel" +str(c)+ ":SCALe?")
+                print("Channel " +str(c)+ " vertical scale: "+qresult)
+                qresult = self.__do_query_string(":CHANnel" +str(c)+ ":OFFSet?")
+                print("Channel " +str(c)+ " offset: "+ qresult)
+                qresult = self.__do_query_string(":CHANnel"+str(c)+":INPut?")
+                print("load imp channel" +str(c)+" : %s" %qresult)
+    
