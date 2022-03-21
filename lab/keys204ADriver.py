65d29a0bc50b5bc3ba61de26738ed12f7cdd6b34
tvbv <thomas.bersani--veroni@ens-paris-saclay.fr>
2022-03-21 12:57:04 +0100

    added data saving and maj doc of keisight driver

--- lab/keys204ADriver.py
+++ lab/keys204ADriver.py
@@ -1,6 +1,10 @@
+"""
+Driver for the Keysight S series 204A, developped at C2N, palaiseau.
+"""
+
 import sys
 import pyvisa as visa
-import time
+import numpy as np
 from comtypes.client import GetModule
 from comtypes.client import CreateObject
 from comtypes.automation import VARIANT
@@ -11,14 +15,54 @@ import comtypes.gen.VisaComLib as VisaComLib
 
 
 class Keys204A():
+    """
+    object that represents the scope
+    
+    ...
+    
+    Attributes
+    ----------
+    adress : str
+        the port adress of the scope
+    ressource_manager : comtypes.POINTER(IResourceManager)
+        the ressource manager for the scope
+    inst : comtypes.POINTER(IFormattedIO488)
+        comtypes instance
+    
+    Methods
+    ----------
+    do_command(command) :
+        sends a command to the scope
+    do_query_string(query) :
+        queries the scope and return the answer as a string
+    do_query_ieee_block(query) :
+        enables querying easily the scope for binary data
+    do_query_number(query) :
+        queries the scope for a number
+    set_trigger(trigger_channel,trigger_sweep,trigger_level) :
+        set the trigger parameters
+    time_base(time_scale, time_ref) :
+        set the time scale on the scope display
+    set_channels(channels, displays, y_scales, offsets, probes, input_couplings) :
+        set the channels display and probes
+    retrieve_waveform(channel) :
+        retrieve a displayed waveform on the specified channel
+    save_waveform(channel, name, path) : 
+        retieve and save a displayed waveform on the specified channel
+    
+    
+    
+    """
     
     def __init__(self, adress, backend = '@ivi'):
         """
         Initializes the instrument with instrument address, backend to use with pyvisa
+        
+        Parameters
         ----------
         inst_address : str
             The port address of the instrument.
-        backend : str
+        backend : str, default : '@ivi'
             Backend to use for pyvisa
         """
         
@@ -33,27 +77,80 @@ class Keys204A():
         self.inst.IO.Clear
         
         
-        print("Connected Device : " + self.__do_query_string("*IDN?"))
+        print("Connected Device : " + self.do_query_string("*IDN?"))
     
-    def __do_command(self,command):
+    def do_command(self,command):
+        """
+        enables prompting a command easily the scope
+        
+        Parameters
+        ----------
+        command : str
+            The command to send to the scope
+        """
         self.inst.WriteString("%s" % command, True)
         self.__check_instrument_errors(command)
         
-    def __do_query_string(self,query):
+    def do_query_string(self,query):
         """
-        private method, enables querying easily the scope
+        enables querying easily the scope
+        
+        Parameters
         ----------
         query : str
             The query to send to the scope
+        
+        Returns
+        ----------
+        string
         """
         self.inst.WriteString("%s" % query, True)
         result = self.inst.ReadString()
         self.__check_instrument_errors(query)
         return result
     
+    def do_query_ieee_block_I2(self,query):
+        """
+        enables querying easily the scope for binary data
+        
+        Parameters
+        ----------
+        query : str
+            The query to send to the scope
+        
+        Returns
+        ----------
+        list
+        """
+        self.inst.WriteString("%s" % query, True)
+        result = self.inst.ReadIEEEBlock(VisaComLib.BinaryType_I2, \
+        False, True)
+        self.__check_instrument_errors(query)
+        return result
+    
+    def do_query_number(self,query):
+        """
+        enables querying easily the scope for a number
+        
+        Parameters
+        ----------
+        query : str
+            The query to send to the scope
+        
+        Returns
+        ----------
+        float
+        """
+        self.inst.WriteString("%s" % query, True)
+        result = self.inst.ReadNumber(VisaComLib.ASCIIType_R8, True)
+        self.__check_instrument_errors(query)
+        return result
+    
     def __check_instrument_errors(self,command):
         """
         private method, enables detection of errors
+        
+        Parameters
         ----------
         command : str
             The command passed to the scope
@@ -77,6 +174,8 @@ class Keys204A():
     def set_trigger(self,trigger_channel = 1,trigger_sweep = None ,trigger_level = None, print_output = 0):
         """
         set the scope's trigger mode, level and channel 
+        
+        Parameters
         ----------
         trigger_sweep : str
             The type of trigger. "TRIGGERED" or "AUTO". By default no changes are applied.
@@ -89,25 +188,27 @@ class Keys204A():
         """
         
         if trigger_sweep != None:
-            self.__do_command(":TRIGger:SWEep "+trigger_sweep)
+            self.do_command(":TRIGger:SWEep "+trigger_sweep)
 
-        self.__do_command(":TRIGger:EDGE:SOURce CHANnel" + str(trigger_channel))
+        self.do_command(":TRIGger:EDGE:SOURce CHANnel" + str(trigger_channel))
         
         if trigger_level !=None:
-            self.__do_command(":TRIGger:LEVel CHANnel" + str(trigger_channel)+","+str(trigger_level))
+            self.do_command(":TRIGger:LEVel CHANnel" + str(trigger_channel)+","+str(trigger_level))
 
         
         if print_output == 1:
-            qresult = self.__do_query_string(":TRIGger:SWEep?")
+            qresult = self.do_query_string(":TRIGger:SWEep?")
             print("Trigger sweep: %s" % qresult)
-            qresult = self.__do_query_string(":TRIGger:EDGE:SOURce?")
+            qresult = self.do_query_string(":TRIGger:EDGE:SOURce?")
             print("Trigger edge source: %s" % qresult)
-            qresult = self.__do_query_string(":TRIGger:LEVel? CHANnel"+ str(trigger_channel))
+            qresult = self.do_query_string(":TRIGger:LEVel? CHANnel"+ str(trigger_channel))
             print("Trigger level, channel" + str(trigger_channel)+": %s" % qresult)
     
     def time_base(self,time_scale, time_ref,print_output = 0):
         """
         set the scope's time base scale and offset
+        
+       Parameters
         ----------
         time_scale : float
             time per division in seconds. ranges from 5e-12 to 20.
@@ -117,21 +218,24 @@ class Keys204A():
             wether to prit or not the new setting to verify all is good
         """
         # Set horizontal scale and offset.
-        self.__do_command(":TIMebase:SCALe " + str(time_scale))
-        self.__do_command(":TIMebase:POSition 0.0")
-        self.__do_command(":TIMebase:REFerence:PERCent " + str(time_ref))
+        self.do_command(":TIMebase:SCALe " + str(time_scale))
+        self.do_command(":TIMebase:POSition 0.0")
+        self.do_command(":TIMebase:REFerence:PERCent " + str(time_ref))
         
         if print_output == 1:
-            qresult = self.__do_query_string(":TIMebase:SCALe?")
+            qresult = self.do_query_string(":TIMebase:SCALe?")
             print("Timebase scale: %s" % qresult)
-            qresult = self.__do_query_string(":TIMebase:POSition?")
+            qresult = self.do_query_string(":TIMebase:POSition?")
             print("Timebase position: %s" % qresult)
-            qresult = self.__do_query_string(":TIMebase:REFerence:PERCent?")
+            qresult = self.do_query_string(":TIMebase:REFerence:PERCent?")
             print(" Timebase reference percent: %s" %qresult)
-                          
+    
+        
     def set_channels(self, channels=[1,2,3,4], displays = [1,1,1,1], y_scales = [1,1,1,1], offsets = [0,0,0,0], probes = [None,None,None,None] , input_couplings = ["DC",'DC','DC','DC'],print_output = 0):
         """
         set the scope's time base scale and offset
+        
+        Parameters
         ----------
         channels : int list
             channels to tweak. 1 to 4.
@@ -160,27 +264,78 @@ class Keys204A():
             c = channels[i]
             print(i)
             if probes[i] != None : 
-                self.__do_command(":CHANnel"+ str(c)+":PROBe "+str(probes[i]))
+                self.do_command(":CHANnel"+ str(c)+":PROBe "+str(probes[i]))
             if displays[i] != None :
-                self.__do_command(":CHANnel"+ str(c)+":DISPlay "+str(displays[i]))
+                self.do_command(":CHANnel"+ str(c)+":DISPlay "+str(displays[i]))
             if y_scales[i] != None :
-                self.__do_command(":CHANnel" +str(c)+ ":SCALe " + str(y_scales[i]))
+                self.do_command(":CHANnel" +str(c)+ ":SCALe " + str(y_scales[i]))
             
             if offsets[i] != None :
-                self.__do_command(":CHANnel" +str(c)+ ":OFFSet "+str(offsets[i]))
+                self.do_command(":CHANnel" +str(c)+ ":OFFSet "+str(offsets[i]))
             
             if input_couplings[i] != None :
-                self.__do_command(":CHANnel"+ str(c)+":INPut "+ input_couplings[i])
+                self.do_command(":CHANnel"+ str(c)+":INPut "+ input_couplings[i])
             
             if print_output == 1:
-                qresult = self.__do_query_string(":CHANnel"+ str(c)+":PROBe?")
+                qresult = self.do_query_string(":CHANnel"+ str(c)+":PROBe?")
                 print("Channel "+ str(c)+" probe attenuation factor: %s" % qresult)
-                qresult = self.__do_query_string(":CHANnel"+ str(c)+":DISPlay?")
+                qresult = self.do_query_string(":CHANnel"+ str(c)+":DISPlay?")
                 print("Channel "+ str(c)+" display: %s" % qresult)
-                qresult = self.__do_query_string(":CHANnel" +str(c)+ ":SCALe?")
+                qresult = self.do_query_string(":CHANnel" +str(c)+ ":SCALe?")
                 print("Channel " +str(c)+ " vertical scale: "+qresult)
-                qresult = self.__do_query_string(":CHANnel" +str(c)+ ":OFFSet?")
+                qresult = self.do_query_string(":CHANnel" +str(c)+ ":OFFSet?")
                 print("Channel " +str(c)+ " offset: "+ qresult)
-                qresult = self.__do_query_string(":CHANnel"+str(c)+":INPut?")
+                qresult = self.do_query_string(":CHANnel"+str(c)+":INPut?")
                 print("load imp channel" +str(c)+" : %s" %qresult)
+
+    def retrieve_waveform(self,channel):
+        """
+        retrieve y and x of the waveform on the specified channel (THEY MUST BE DISPLAYED)
+        
+        Parameters
+        ----------
+        channel : int
+            number of channel to retrieve the waveform from
+        
+        Return
+        ----------
+        x, y : numpy arrays
+        """
+        
+        self.do_command(":DIGitize")
+        
+        self.do_command(":WAVeform:SOURce channel"+str(channel))
+        self.do_command(":WAVeform:FORMat word")
+        self.do_command(":SYSTem:HEADer OFF")
+        self.do_command(":WAVeform:STReaming OFF")
+        number_of_points=self.do_query_string(":WAVeform:POINts?")
+        y_increment = self.do_query_number(":WAVeform:YINCrement?")
+        y_origin = self.do_query_number(":WAVeform:YORigin?")
+        x_increment = self.do_query_number(":WAVeform:XINCrement?")
+        x_origin = self.do_query_number(":WAVeform:XORigin?")
+
+        waveform_raw = self.do_query_ieee_block_I2(":WAVeform:DATA?")
+        self.do_command(":RUN")
+        y_waveform = y_increment*np.array(waveform_raw) +y_origin
+        x_waveform = x_increment*np.array(range(int(number_of_points)))+x_origin
+        
+        return x_waveform, y_waveform
     
+    def save_waveform(self, channel, name = 'waveform',path = ''):
+        """
+        retrieve y and x of the waveform on the specified channel (THEY MUST BE DISPLAYED) and store thame with the specified name within the specified folder.
+        The data is stored under numpy array format with x being data[:,0] and y being data[:,1]
+        
+        Parameters
+        ----------
+        channel : int
+            number of channel to retrieve the waveform from
+        path : str
+            path to where to store the data
+        name : str
+            name under wich data will be stored
+        """
+        
+        x,y = self.retrieve_waveform(channel)
+        data_to_store = np.stack((x,y), axis=1)
+        np.save(path + name+'.npy',data_to_store)
