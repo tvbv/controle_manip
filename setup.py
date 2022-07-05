acd7140e80c9b7f566d7436dd1f2858a5245c91d
Thomas Vincenzo Bersani--Veroni <adrien.renaudineau@universite-paris-saclay.fr>
2022-07-05 18:06:19 +0200

    Create pip-ready branch
    Create setup.py + remove useless files for usage

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='controle_manip',
+	version='0.0.1',
+	packages=find_packages(),
+	install_requires=[
+		'numpy == 1.19.2',
+    'comtypes == 1.1.7',
+    'pyvisa ==  1.11.3',
+	]
+)
